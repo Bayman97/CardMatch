e0ee0a958d609304c613b6e3910e3b8f718f7c8c
MIchael <74796912+Bayman97@users.noreply.github.com>
2020-11-20 22:25:37 -0800

    added card layout and cards now flipped when tapped

---
+++ CardMatch/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  CardMatch
+//
+//  Created by Michael Zeng on 2020-11-20.
+//
+
+import Foundation
---
+++ CardMatch/CardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CardCollectionViewCell.swift
+//  CardMatch
+//
+//  Created by Michael Zeng on 2020-11-20.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
